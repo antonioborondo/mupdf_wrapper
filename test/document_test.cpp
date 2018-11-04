4a6f3589dbff0140066d7018ed46cad79d2e32f3
Antonio Borondo <antonioborondo@users.noreply.github.com>
2018-11-04 18:14:46 +0000

    CTest integration (#6)

--- test/document_test.cpp
+++ test/document_test.cpp
@@ -5,17 +5,47 @@
 
 #include <memory>
 
-TEST_CASE("Create Document throws exception if document handlers are not registered", "[document]")
-{
-    const auto context = std::make_shared<mupdf_wrapper::Context>();
-
-    REQUIRE_THROWS_AS(mupdf_wrapper::Document(context, ""), std::runtime_error);
-}
-
-TEST_CASE("Create Document throws exception if filename is empty", "[document]")
+TEST_CASE("GIVEN no document WHEN create new document THEN an exception is thrown", "[document]")
 {
     const auto context = std::make_shared<mupdf_wrapper::Context>();
     context->register_document_handlers();
 
     REQUIRE_THROWS_AS(mupdf_wrapper::Document(context, ""), std::runtime_error);
+}
+
+TEST_CASE("GIVEN one page empty document WHEN create new document THEN document is created without throwing exception", "[document]")
+{
+    const auto context = std::make_shared<mupdf_wrapper::Context>();
+    context->register_document_handlers();
+
+    std::shared_ptr<mupdf_wrapper::Document> document;
+    REQUIRE_NOTHROW(document = std::make_shared<mupdf_wrapper::Document>(context, "one_page_empty_document.pdf"));
+
+    const auto mupdf_document = document->get();
+    REQUIRE(nullptr != mupdf_document);
+}
+
+TEST_CASE("GIVEN one page empty document WHEN create new document without register document handlers THEN an exception is thrown", "[document]")
+{
+    const auto context = std::make_shared<mupdf_wrapper::Context>();
+
+    REQUIRE_THROWS_AS(mupdf_wrapper::Document(context, "one_page_empty_document.pdf"), std::runtime_error);
+}
+
+TEST_CASE("GIVEN one page empty document WHEN get total pages THEN total pages equal to 1", "[document]")
+{
+    const auto context = std::make_shared<mupdf_wrapper::Context>();
+    context->register_document_handlers();
+
+    const auto document = std::make_shared<mupdf_wrapper::Document>(context, "one_page_empty_document.pdf");
+    REQUIRE(document->get_total_pages() == 1);
+}
+
+TEST_CASE("GIVEN five pages empty document WHEN get total pages THEN total pages equal to 5", "[document]")
+{
+    const auto context = std::make_shared<mupdf_wrapper::Context>();
+    context->register_document_handlers();
+
+    const auto document = std::make_shared<mupdf_wrapper::Document>(context, "five_pages_empty_document.pdf");
+    REQUIRE(document->get_total_pages() == 5);
 }
