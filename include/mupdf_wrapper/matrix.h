24f1aa6f763616ef1639b33b946ad9fa27cd1df3
Antonio Borondo <antonioborondo@users.noreply.github.com>
2018-12-20 21:45:25 +0000

    Replace raw pointer by smart pointer inside Matrix (#8)

--- include/mupdf_wrapper/matrix.h
+++ include/mupdf_wrapper/matrix.h
@@ -5,15 +5,16 @@
 
 #include <mupdf/fitz.h>
 
+#include <memory>
+
 namespace mupdf_wrapper
 {
     class MUPDF_WRAPPER_API Matrix
     {
-        fz_matrix* m_mupdf_matrix;
+        std::unique_ptr<fz_matrix> m_mupdf_matrix;
 
     public:
         Matrix();
-        ~Matrix();
         const fz_matrix* get() const;
         void set_zoom(unsigned int zoom) const;
         void set_rotation(float rotation) const;
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -3,29 +3,21 @@
 namespace mupdf_wrapper
 {
     Matrix::Matrix()
-        : m_mupdf_matrix(nullptr)
-    {
-        m_mupdf_matrix = new fz_matrix();
-    }
-
-    Matrix::~Matrix()
-    {
-        delete m_mupdf_matrix;
-        m_mupdf_matrix = nullptr;
-    }
+        : m_mupdf_matrix(std::make_unique<fz_matrix>())
+    {}
 
     const fz_matrix* Matrix::get() const
     {
-        return m_mupdf_matrix;
+        return m_mupdf_matrix.get();
     }
 
     void Matrix::set_zoom(unsigned int zoom) const
     {
-        fz_scale(m_mupdf_matrix, zoom/100.f, zoom/100.f);
+        fz_scale(m_mupdf_matrix.get(), zoom/100.f, zoom/100.f);
     }
 
     void Matrix::set_rotation(float rotation) const
     {
-        fz_rotate(m_mupdf_matrix, rotation);
+        fz_rotate(m_mupdf_matrix.get(), rotation);
     }
 }
